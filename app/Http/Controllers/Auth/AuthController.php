25984f3f0afe13381eec7e23b06eab93f58d16a7
aymansohag <aymansohag.contact@gmail.com>
2023-02-21 14:42:19 +0600

    social app id and secret put env

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -8,5 +8,33 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    // Google login
+    public function redirectToGoogle(){
+        return Socialite::driver('google')->redirect();
+    }
+    
+    // Google callback
+    public function handleGooleCallback(){
+        $user = Socialite::driver('google')->user();
+    }
 
+    // Facebook login
+    public function redirectToFacebook(){
+        return Socialite::driver('facebook')->redirect();
+    }
+    
+    // Facebook callback
+    public function handleFacebookCallback(){
+        $user = Socialite::driver('facebook')->user();
+    }
+
+     // Github login
+     public function redirectToGithub(){
+        return Socialite::driver('github')->redirect();
+    }
+    
+    // Github callback
+    public function handleGithubCallback(){
+        $user = Socialite::driver('github')->user();
+    }
 }
